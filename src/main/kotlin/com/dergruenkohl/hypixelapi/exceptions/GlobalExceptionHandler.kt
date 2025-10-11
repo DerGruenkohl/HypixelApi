b4a3cb95013fb6da9e75040a30f22a5e77876077
DerGruenkohl <karlbaum100@gmail.com>
2025-10-11 20:48:46 +0200

    add JsonConvertException

--- src/main/kotlin/com/dergruenkohl/hypixelapi/exceptions/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/dergruenkohl/hypixelapi/exceptions/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.dergruenkohl.hypixelapi.exceptions
 
+import io.ktor.serialization.JsonConvertException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+
     @ExceptionHandler(HypixelRequestException::class)
     fun handleHypixelRequestException(ex: HypixelRequestException): ResponseEntity<String> {
         return ResponseEntity(ex.message, HttpStatus.valueOf(ex.statusCode.value))
+    }
+    @ExceptionHandler(JsonConvertException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleJsonConvertException(ex: JsonConvertException): String {
+        return "Could not parse response from the Hypixel API Please report this to the developer."
     }
 }
