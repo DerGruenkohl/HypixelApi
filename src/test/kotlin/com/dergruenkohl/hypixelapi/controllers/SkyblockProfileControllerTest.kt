507e17762b99625635aa2b2659d2903494cbe0f5
DerGruenkohl <karlbaum100@gmail.com>
2025-10-12 13:19:02 +0200

    SkyblockProfileControllerTest.kt

--- src/test/kotlin/com/dergruenkohl/hypixelapi/controllers/SkyblockProfileControllerTest.kt
+++ src/test/kotlin/com/dergruenkohl/hypixelapi/controllers/SkyblockProfileControllerTest.kt
@@ -5,6 +5,8 @@ import com.dergruenkohl.hypixelapi.TestConfiguration
 import com.dergruenkohl.hypixelapi.client.HttpClient
 import com.dergruenkohl.hypixelapi.client.config.CacheConfig
 import com.dergruenkohl.hypixelapi.client.data.SkyblockProfile
+import com.dergruenkohl.hypixelapi.client.data.SkyblockProfileMember
+import com.dergruenkohl.hypixelapi.data.Skills
 import com.dergruenkohl.hypixelapi.services.SkyblockService
 import com.dergruenkohl.hypixelapi.services.UUIDService
 import com.dergruenkohl.hypixelapi.services.impl.SkyblockServiceImpl
@@ -44,6 +46,39 @@ class SkyblockProfileControllerTest @Autowired constructor(
             .exchange()
             .expectStatus().isOk
             .expectBodyList(SkyblockProfile::class.java)
+    }
+    @Test
+    fun testProfileEndpointReturnsProfile() {
+        webClient.get()
+            .uri("/v1/profiles/garaf/selected")
+            .header("API-Key", apiKey)
+            .header("User-Agent", "HypixelApi-WrapperTest")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(SkyblockProfile::class.java)
+    }
+    @Test
+    fun testProfileMemberEndpointReturnsProfileMember() {
+        webClient.get()
+            .uri("/v1/profiles/garaf/selected/member")
+            .header("API-Key", apiKey)
+            .header("User-Agent", "HypixelApi-WrapperTest")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(SkyblockProfileMember::class.java)
+    }
+    @Test
+    fun testSkillEndpointReturnsSkill() {
+        webClient.get()
+            .uri("/v1/skills/garaf")
+            .header("API-Key", apiKey)
+            .header("User-Agent", "HypixelApi-WrapperTest")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(Skills::class.java).value {
+                val exp = it?.combat?.experience
+                assert(exp != null && exp > 0)
+            }
     }
 
 }
